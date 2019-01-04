1710f37b2139a05831a8cadfeb131af8df04419e
M-Nasuk <m.kusanagi@gmx.fr>
2019-01-05 00:37:15 +0100

    !

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
 
 getData('./header.php', prependBody);
-for (let i = 0; i < 5; i++) {
-  getData('./post-desc.php', htmlP);
-  getData('./post.php', prependMain);
-}
-getData('./post.php', appendSection);
 
+for (let i = 0; i < 5; i++) {
+  getData('./post.php', prependMain);
+  getData('./post-desc.php', htmlP);
+}
+
+getData('./post.php', appendSection);
 
 
 })
