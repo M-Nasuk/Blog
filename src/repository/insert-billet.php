99626ad2d2f3122365150d7b8882fcf62fe05427
M-Nasuk <m.kusanagi@gmx.fr>
2019-01-08 13:01:22 +0100

    !!

--- src/repository/insert-billet.php
+++ src/repository/insert-billet.php
@@ -1,14 +1,30 @@
 <?php
 include 'db.php';
 
+$data_billet = [
+  'title' => $titre,
+  'corps' => $corps_texte,
+  'date' => $date_pub,
+  'cat' => $categorie,
+  'author' => $author
+];
+
 $insert_billet = $channel->prepare
 (
-  'INSERT INTO billet (titre) 
-  VALUES ('?');');
+  'INSERT INTO billet (titre, corps_de_texte, date_de_publication, categorie, author)
+  VALUES (:titre, :corps_texte, :date_pub, ,:categorie, :author);
+');
+
+if (isset ($_POST)) {
+
+}
+
+
+
+$insert_billet->execute($data_billet);
 
 
 
 
-
-
+header('Location: ../billet.php');
  ?>
