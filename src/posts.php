e05557f7b27d410c33b2e761ccdf05b596f02bfc
M-Nasuk <m.kusanagi@gmx.fr>
2019-01-08 16:30:45 +0100

    Images upload & display

--- src/posts.php
+++ src/posts.php
@@ -1,10 +1,11 @@
 <?php include "./repository/select-all.php"; ?>
 <?php include "./utilities.php"; ?>
 
+
 <?php foreach ($posts_results as $result_id): ?>
   <?php $date = changeMonth($result_id['date_de_publication']); ?>
   <article class="art">
-    <img src="src/forest.jpg" alt="img_post_forest">
+    <img src="<?php $image = $result_id['titre'].'.jpeg'; file_put_contents('../public/images/'.$image, stripslashes($result_id['image'])); echo './public/images/'.$result_id['titre'].'.jpeg'; ?>" alt="img_post_forest">
     <h2><a href="single-post.php?id=<?= $result_id['id_billet']; ?>"><?= $result_id['titre']; ?></a></h2>
     <div>
       <h3><?= str_replace($date[0], $date[1], date('d F y \a H:i',strtotime($result_id['date_de_publication']))); ?> | </h3>
--- src/repository/insert-billet.php
+++ src/repository/insert-billet.php
@@ -1,30 +1,30 @@
 <?php
 include 'db.php';
 
-$data_billet = [
-  'title' => $titre,
-  'corps' => $corps_texte,
-  'date' => $date_pub,
-  'cat' => $categorie,
-  'author' => $author
-];
+$titre = $_POST['title'];
+$corps_texte = $_POST['subject'];
+$date_pub = $_POST['datetime'];
+$img_blob = addslashes(file_get_contents($_FILES['img']['tmp_name']));
+$video = null;
+$categorie = $_POST['categorie'];
+$author = $_POST['author'];
+
+
 
 $insert_billet = $channel->prepare
 (
-  'INSERT INTO billet (titre, corps_de_texte, date_de_publication, categorie, author)
-  VALUES (:titre, :corps_texte, :date_pub, ,:categorie, :author);
+  'INSERT INTO billet (titre, corps_de_texte, image, video, date_de_publication, categorie, author)
+  VALUES (?, ?, ?, ?, ?, ?, ?);
 ');
 
-if (isset ($_POST)) {
-
-}
 
 
 
-$insert_billet->execute($data_billet);
+$insert_billet->execute([$titre, $corps_texte, $img_blob, $video, $date_pub, $categorie, $author]);
 
 
 
+header('Location: ../../index.php');
+//include '../../template/test.phtml';
 
-header('Location: ../billet.php');
  ?>
