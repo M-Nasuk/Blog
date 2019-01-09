22cfcd292e2fc77dac7e0eb1f5b384b3e03a1d09
M-Nasuk <m.kusanagi@gmx.fr>
2019-01-09 01:11:46 +0100

    !

--- src/repository/insert-user.php
+++ src/repository/insert-user.php
@@ -9,11 +9,26 @@ $pw = $_POST['pwd'];
 $email = $_POST['e_mail'];
 $role = 2;
 $phone = $_POST['phoneNumber'];
-$avatar = $_POST['avatar'];
+$avatar = null;
 
+$data_user = [
+  'nom' => $nom,
+  'prenom' => $prenom,
+  'pseudo' => $pseudo,
+  'pw' => $pw,
+  'email' => $email,
+  'role' => $role,
+  'phone' => $phone,
+  'avatar' => $avatar
+];
 
+$insert_user = $channel->prepare
+(
+  'INSERT INTO utilisateur (nom, prenom, pseudo, mot_de_passe, e_mail, telephone, role, avatar)
+  VALUES (:nom, :prenom, :pseudo, :pw, :email, :role, :phone, :avatar);
+');
 
-
+$insert_user->execute([$nom,$prenom,$pseudo,$pw,$email,$role,$phone,$avatar]);
 
 
 
