ade1bea9109672ae8c533d295d5c34ae38b97dd8
M-Nasuk <m.kusanagi@gmx.fr>
2019-01-14 10:57:49 +0100

    !

--- src/header.php
+++ src/header.php
@@ -5,11 +5,18 @@ if (isset($_SESSION['user_data'])) {
   $_SESSION['test'] = null;
 } ?>
 <header id="header">
+  <?php if($_SESSION['test'] && $_SESSION['user_data']['role'] == 0): ?>
+    <a href="./admin.php" id="admin"><h2>Admin</h2></a>
+  <?php endif; ?>
   <h1>Blog</h1>
   <nav id="h_nav">
     <ul>
       <li><a href="index.php"><h2>Accueil</h2></a></li>
-      <li><a href="billet.php"><h2>Billets</h2></a></li>
+
+      <?php if($_SESSION['test'] && $_SESSION['user_data']['role'] == 0 || $_SESSION['test'] && $_SESSION['user_data']['role'] == 1): ?>
+        <li><a href="billet.php"><h2>Billets</h2></a></li>
+      <?php endif; ?>
+
       <?php if (!$_SESSION['test']): ?>
         <li><a href="connexion.php"><h2>Connexion</h2></a></li>
       <?php else: ?>
