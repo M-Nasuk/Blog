a548462f79817d3ed97072e4f3298b65759a3ec1
M-Nasuk <m.kusanagi@gmx.fr>
2019-01-15 18:34:11 +0100

    !!

--- admin/main.js
+++ admin/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
 
   $.getJSON('admin/data.php', function(data) {
+    //.appendTo('select[name="billet"]');
     for (let i = 0; i < data['posts'].length; i++) {
-      $('<option>').val(i).html(JSON.parse(data['posts'][i])['titre']).appendTo('select[name="billet"]');
+      $(`<option>`).val(i).html(JSON.parse(data['posts'][i])['titre']).appendTo('select[name="billet"]');
     }
   });
+  $('select[name="billet"] option[value=0]').attr('selected','selected');
 
   $('select[name="billet"]').change(function() {
     $.getJSON('admin/data.php', function(data) {
@@ -20,17 +22,21 @@ $(document).ready(function () {
 
       $('<li>').html(
         $('<textarea rows="4">').val(JSON.parse(data['posts'][$('select[name="billet"]').val()])['corps_de_texte'])
-      ).prepend($('<label>').html('Titre :')).appendTo('#fd ul');
+      ).prepend($('<label>').html('Corps :')).appendTo('#fd ul');
 
       $('<li>').html(
         $('<input type="file">'))
-        .prepend($('<label>').html('Titre :'))
+        .prepend($('<label>').html('Image :'))
         .append($('<img>').attr('src', 'public/images/'+JSON.parse(data['posts'][$('select[name="billet"]').val()])['titre']+'.jpeg'))
         .appendTo('#fd ul');
 
-      /*$('<li>').html(
-        $('<select name="cat">')
-      )*/
+      $('<li>').html(
+        $('<select name="cat">'))
+        .prepend($('<label>').html('Categorie :'))
+        .appendTo('#fd ul');
+      for (let i = 0; i < data['posts'].length; i++) {
+        $('<option value>').val(i).html(JSON.parse(data['cat'][i])['nom']).appendTo('select[name="cat"]');
+      }
 
 
 
