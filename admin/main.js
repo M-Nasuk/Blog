c2b9c73b292d8f1b9708d5375f0d6df67abfe345
M-Nasuk <m.kusanagi@gmx.fr>
2019-01-24 15:26:35 +0100

    !!

--- admin/main.js
+++ admin/main.js
@@ -14,93 +14,10 @@ $(document).ready(function () {
     }
   });
 
-  $('select[name="billet"]').change(function() {
-    $.getJSON('admin/data.php', function(data) {
-      $('#fd ul').html('');
-      $('<li>').html(
-        $('<input type="text" name="billet_id">').val(JSON.parse(data['posts'][$('select[name="billet"]').val()])['id_billet'])
-      ).prepend($('<label>').html('ID :')).appendTo('#fd ul');
+  $('select[name="billet"]').change(showPost);
 
-      $('<li>').html(
-        $('<input type="text" name="billet_titre">').val(JSON.parse(data['posts'][$('select[name="billet"]').val()])['titre'])
-      ).prepend($('<label>').html('Titre :')).appendTo('#fd ul');
+  $('select[name="user"]').change(showUser);
 
-      $('<li>').html(
-        $('<textarea rows="4" name="billet_cdt">').val(JSON.parse(data['posts'][$('select[name="billet"]').val()])['corps_de_texte'])
-      ).prepend($('<label>').html('Corps :')).appendTo('#fd ul');
 
-      $('<li>').html(
-        $('<input type="file" name="billet_img" accept="image/*">'))
-        .prepend($('<label>').html('Image :'))
-        .append($('<img>').attr('src', 'public/images/'+JSON.parse(data['posts'][$('select[name="billet"]').val()])['titre']+'.jpeg'))
-        .appendTo('#fd ul');
-
-      $('<li>').html(
-        $('<select name="categorie">'))
-        .prepend($('<label>').html('Categorie :'))
-        .appendTo('#fd ul');
-
-      $('<li>').html(
-        $('<button type="submit" style="display: block; margin: 1rem auto; font-size: 2em;">Modifier</button>'))
-        .appendTo('#fd ul');
-
-      for (let i = 0; i < data['cat'].length; i++) {
-        $('<option>').val(i).html(JSON.parse(data['cat'][i])['nom']).appendTo('select[name="categorie"]');
-      }
-    });
-  });
-
-  $('select[name="user"]').change(function() {
-    $.getJSON('admin/data.php', function(data) {
-      $('#fd_user ul').html('');
-      $('<li>').html(
-        $('<input type="text">').val(JSON.parse(data['users'][$('select[name="user"]').val()])['nom'])
-      ).prepend($('<label>').html('Nom :')).appendTo('#fd_user ul');
-
-      $('<li>').html(
-        $('<input type="text">').val(JSON.parse(data['users'][$('select[name="user"]').val()])['prenom'])
-      ).prepend($('<label>').html('Prenom :')).appendTo('#fd_user ul');
-
-      $('<li>').html(
-        $('<input type="text">').val(JSON.parse(data['users'][$('select[name="user"]').val()])['pseudo'])
-      ).prepend($('<label>').html('Pseudo :')).appendTo('#fd_user ul');
-
-      $('<li>').html(
-        $('<input type="text">').val(JSON.parse(data['users'][$('select[name="user"]').val()])['e_mail'])
-      ).prepend($('<label>').html('Email :')).appendTo('#fd_user ul');
-
-      $('<li>').html(
-        $('<input type="text">').val(JSON.parse(data['users'][$('select[name="user"]').val()])['telephone'])
-      ).prepend($('<label>').html('Telephone :')).appendTo('#fd_user ul');
-
-      $('<li>').html(
-        $('<input type="file">'))
-        .prepend($('<label>').html('Image :'))
-        .append($('<img>').attr('src', 'public/user/'+JSON.parse(data['users'][$('select[name="user"]').val()])['pseudo']+'.jpeg'))
-        .appendTo('#fd_user ul');
-
-      $('<li>').html(
-        $('<label>').html('Role : '+JSON.parse(data['users'][$('select[name="user"]').val()])['role'])
-      ).appendTo('#fd_user ul');
-
-      $('<li>').html(
-        $('<select name="role">'))
-        .prepend($('<label>').html('New role :'))
-        .appendTo('#fd_user ul');
-
-      $('<li>').html(
-        $('<button type="submit" style="display: block; margin: 1rem auto; font-size: 2em;">Modifier</button>'))
-        .appendTo('#fd_user ul');
-
-      for (let i = 0; i < data['roles'].length; i++) {
-        $('<option>').val(i).html(JSON.parse(data['roles'][i])['titre']).appendTo('select[name="role"]');
-      }
-    });
-  });
-
-  $('input[name="billet_img"]').change(function() {
-    $('input[name="billet_img"] + img').after('<p>').html($('input[name="billet_img"]').val());
-    //console.log($(this).val());
-  });
 
 })
--- admin/repository/update.php
+++ admin/repository/update.php
@@ -1,17 +1,45 @@
 <?php
 
 include '../../src/repository/db.php';
+var_dump($_FILES);
+var_dump($_POST);
 
 if (isset($_POST)) {
-  foreach ($_POST as $key => $val) {
-    echo '<p>'.var_dump($key).var_dump($val).'</p>';
+  $titre = $_POST['billet_titre'];
+  $corps_texte = $_POST['billet_cdt'];
+  $categorie = $_POST['categorie'];
+  $id = $_POST['id'];
+
+  if (!empty($_FILES['billet_img']['name'])) {
+
+    $img_blob = addslashes(file_get_contents($_FILES['billet_img']['tmp_name']));
+
+    $update_post = $channel->prepare
+    (
+      'UPDATE billet
+      SET titre = ?, corps_de_texte = ?, image = ?, categorie = ?
+      WHERE id_billet = ?;
+    ');
+
+    $update_post->execute([$titre, $corps_texte, $img_blob, $categorie, $id]);
+
+
+  } else {
+
+    $update_post = $channel->prepare
+    (
+      'UPDATE billet
+      SET titre = ?, corps_de_texte = ?, categorie = ?
+      WHERE id_billet = ?;
+    ');
+
+    $update_post->execute([$titre, $corps_texte, $categorie, $id]);
+
+
   }
-}
 
-if (isset($_FILES)) {
-  var_dump($_FILES);
-}
 
+}
 
 
 
