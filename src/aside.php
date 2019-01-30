6bed85af569247455fa6e8537472fed15e965f53
M-Nasuk <m.kusanagi@gmx.fr>
2019-01-30 16:16:57 +0100

    add jump

--- src/aside.php
+++ src/aside.php
@@ -1,8 +1,11 @@
 <?php include "./repository/select-all.php"; ?>
 
-<h2 style="padding-left: 1rem; font-weight: normal; margin-bottom: 1rem;">Categorie</h3>
-<ul style="padding-left: 2rem;">
+<h2 style="padding-left: 1rem; font-weight: normal; margin-bottom: 1rem;">Categorie</h2>
+<ul style="padding-left: 2rem; margin-bottom: 2rem;">
   <?php foreach ($cat_results as $cat_index): ?>
     <li style="margin-bottom: 0.5rem;"><h3 style="font-weight: normal;"><a href="result.php?id=<?= $cat_index['id_categorie']; ?>"><?= $cat_index['nom']; ?></a></h3></li>
   <?php endforeach; ?>
 </ul>
+
+<h2 style="padding-left: 1rem; font-weight: normal; margin-bottom: 1rem;">Game</h2>
+<h3 style="padding-left: 2rem; font-weight: normal;"><a href="jump.php">Jump</a></h3>
