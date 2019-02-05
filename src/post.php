f3123c071270236bbb42237058e124b01563e36e
M-Nasuk <m.kusanagi@gmx.fr>
2019-02-05 19:32:02 +0100

    !!

--- src/post.php
+++ src/post.php
@@ -9,8 +9,8 @@
   <h2><?= $single_post_result['titre']; ?></h2>
   <div>
     <h3><?= str_replace($date[0], $date[1], date('d F y H:i',strtotime($single_post_result['date_de_publication']))); ?> | </h3>
-    <h3><a href="#"><?= $single_post_result['pseudo']; ?> | </a></h3>
-    <h3><a href="#"><?= $single_post_result['nom']; ?></a></h3>
+    <h3><!--a href="#"--><?= $single_post_result['pseudo']; ?> | <!--/a--></h3>
+    <h3><a href="result.php?id=<?= $single_post_result['categorie']; ?>"><?= $single_post_result['nom']; ?></a></h3>
   </div>
     <p><?= $single_post_result['corps_de_texte']; ?></p>
 </article>
--- src/posts.php
+++ src/posts.php
@@ -11,7 +11,7 @@
     <h2><a href="single-post.php?id=<?= $result_id['id_billet']; ?>"><?= $result_id['titre']; ?></a></h2>
     <div>
       <h3><?= str_replace($date[0], $date[1], date('d F y \a H:i',strtotime($result_id['date_de_publication']))); ?> | </h3>
-      <h3><a href="#"><?= $result_id['pseudo']; ?> | </a></h3>
+      <h3><!--a href="#"--><?= $result_id['pseudo']; ?> | <!--/a--></h3>
       <h3><a href="result.php?id=<?= $result_id['categorie']; ?>"><?= $result_id['nom']; ?></a></h3>
     </div>
     <p><?= substr($result_id['corps_de_texte'], 0, 50);?>...</p>
